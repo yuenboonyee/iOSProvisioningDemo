fb43f37b8f2ff1f1e65f7cd870d7546a220f7a18
Yuen Boon Yee <yuen_boon_yee@sereneit.com>
2014-05-24 17:54:36 +1000

    Added new unit test.

--- iOSProvisioningDemoTests/iOSProvisioningDemoTests.m
+++ iOSProvisioningDemoTests/iOSProvisioningDemoTests.m
@@ -28,7 +28,11 @@
 
 - (void)testExample
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    //XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
 }
 
+- (void)testExample2
+{
+    //XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
 @end
